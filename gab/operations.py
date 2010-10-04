71679bbf78652604156028e14d50a9ca34f7631b
Gert Van Gool <gertvangool@gmail.com>
2010-10-04 13:25:33 +0200

    Added cmd to change apt-proxy

--- gab/operations.py
+++ gab/operations.py
@@ -14,3 +14,12 @@ def set_hostname(new_hostname):
     'Set the hostname on the server'
     sudo('echo %s > /etc/hostname' % new_hostname)
     append('127.0.0.1\t%s' % new_hostname, '/etc/hosts', use_sudo=True)
+
+
+def set_apt_proxy(host):
+    '''
+    Set the APT proxy.
+
+    ``host`` should be something like http://localhost:3142
+    '''
+    sudo('echo Acquire::http::Proxy "%s";  > /etc/apt/apt.conf.d/01proxy' % host)
