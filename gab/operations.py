74b45e2890d4f77a195a3ba92ae97b8f0bc5545b
Gert Van Gool <gertvangool@gmail.com>
2010-10-04 14:06:16 +0200

    Added commands to create and delete users

--- gab/operations.py
+++ gab/operations.py
@@ -23,3 +23,31 @@ def set_apt_proxy(host):
     ``host`` should be something like http://localhost:3142
     '''
     sudo('echo Acquire::http::Proxy "%s";  > /etc/apt/apt.conf.d/01proxy' % host)
+
+
+def create_user(username, password='password'):
+    '''
+    Create a new user.
+
+    ``username``
+        the new user to create
+    ``password``
+        optionally the password for the user, default is ``password``
+    '''
+    # create user
+    sudo('useradd -U -m -s /bin/bash %s' % username)
+    # set password
+    sudo('echo "%s:%s" | chpasswd' % (username,password))
+    # add to admin group (sudoers)
+    sudo('adduser %s admin' % username)
+    # add to adm group (administrators)
+    sudo('adduser %s adm' % username)
+
+
+def delete_user(username):
+    'Delete a user and its group'
+    sudo('deluser --remove-home %s' % username)
+    try:
+        sudo('delgroup --only-if-empty %s' % username)
+    except:
+        pass
