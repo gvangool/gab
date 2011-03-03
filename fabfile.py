5e8f0f3cf8c1d0f30cd16a043df3ba6b0fa076e1
Gert Van Gool <gertvangool@gmail.com>
2011-03-03 12:55:21 +0100

    Enable Server Density in our default fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,5 +3,6 @@ from fabric.api import *
 from gab.install import *
 from gab.maintenance import *
 from gab.operations import *
+from gab.server_density import *
 from gab.services import *
 from gab.setup import *
